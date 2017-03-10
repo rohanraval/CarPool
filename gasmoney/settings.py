c49039d0e195910c0d55c4e666702127f826df4e
Rohan Raval <Rohan@Rohans-MacBook-Pro.local>
2017-03-09 19:39:56 -0500

    fixed settings.py error

--- gasmoney/settings.py
+++ gasmoney/settings.py
@@ -124,6 +124,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) #
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
@@ -131,13 +132,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') #
 STATIC_URL = '/static/'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) #
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 ) #
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' # 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' #
 
 
 # import dj_database_url
