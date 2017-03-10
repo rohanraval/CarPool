365fc3392ffb88836d95f29c94c98bab1d1de737
Rohan Raval <Rohan@Rohans-MacBook-Pro.local>
2017-03-09 20:44:31 -0500

    db errors in settings fixed

--- gasmoney/settings.py
+++ gasmoney/settings.py
@@ -76,19 +76,19 @@ WSGI_APPLICATION = 'gasmoney.wsgi.application'
 
 DATABASES = {
     # FOR SQLITE3:
-	# 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-	# FOR POSTGRES:
 	'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fuelcalc',
-        'USER': 'fuelcalc_admin',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+	# FOR POSTGRES:
+	# 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'fuelcalc',
+    #     'USER': 'fuelcalc_admin',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': '5433',
+    # }
 }
 
 
